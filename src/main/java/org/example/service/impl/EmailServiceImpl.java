adca063adc37f191ae3e77234eb780dab8202976
poornima <poornimasrn1@gmail.com>
2023-10-04 22:46:07 +0530

    Deleted

--- src/main/java/org/example/service/impl/EmailServiceImpl.java
+++ src/main/java/org/example/service/impl/EmailServiceImpl.java
@@ -17,7 +17,7 @@ public class EmailServiceImpl implements EmailService {
 
     @Autowired
     JavaMailSender javaMailSender;
-
+*
     private String sender;
     @Override
     public String sendMail(EmailReqDTO emailReqDTO) {
